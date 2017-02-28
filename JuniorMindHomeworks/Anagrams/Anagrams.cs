3bcc78cee12e98a0b07b3d34f18eb435c61fe972
raresJM <rares.scarlat.jm@gmail.com>
2017-03-01 00:06:44 +0200

    added Anagrams project

---
+++ JuniorMindHomeworks/Anagrams/Anagrams.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrams
+{
+    [TestClass]
+    public class Anagrams
+    {
+        [TestMethod]
+        public void Test_ABC()
+        {
+            Assert.AreEqual(6,returnNoOfAnagrams("ABC"));
+        }
+    }
+}
