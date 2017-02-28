f0fc972fd932e9130f500a52a54100658da3d479
raresJM <rares.scarlat.jm@gmail.com>
2017-02-28 20:34:05 +0200

    corrected returnColumnLetters() return type to String

---
+++ JuniorMindHomeworks/Excel/Excel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excel
+{
+    [TestClass]
+    public class Excel
+    {
+        [TestMethod]
+        public void ExcelTest_01()
+        {
+            Assert.AreEqual("A",returnColumnLetters(1));
+        }
+        [TestMethod]
+        public void ExcelTest_02()
+        {
+            Assert.AreEqual("Z", returnColumnLetters(26));
+        }
+        public String returnColumnLetters(int number)
+        {
+                
+        }
+    }
+}
