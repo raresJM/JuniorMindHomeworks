aed4920a598b4531a7a58f8f7da2fa7b60fddcc9
raresJM <rares.scarlat.jm@gmail.com>
2017-02-28 20:51:59 +0200

    corrected returnColumnLetters() return type to char. wrote the returnColumnLetters() method to return correctly for A and Z

--- JuniorMindHomeworks/Excel/Excel.cs
+++ JuniorMindHomeworks/Excel/Excel.cs
@@ -9,16 +9,21 @@ namespace Excel
         [TestMethod]
         public void ExcelTest_01()
         {
-            Assert.AreEqual("A",returnColumnLetters(1));
+            Assert.AreEqual('A',returnColumnLetters(1));
         }
         [TestMethod]
         public void ExcelTest_02()
         {
-            Assert.AreEqual("Z", returnColumnLetters(26));
+            Assert.AreEqual('Z',returnColumnLetters(26));
         }
-        public String returnColumnLetters(int number)
+        public char returnColumnLetters(int number)
         {
-                
+            char[] chars = new char[26];
+            for (int i = 0; i < 26; i++)
+            {
+                chars[i] = (char)('A' + i);
+            }
+            return chars[number-1];         
         }
     }
 }
