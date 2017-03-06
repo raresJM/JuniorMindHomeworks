c9e79b24508b6dd63b6b2c6a3531b5537f05f54a
raresJM <rares.scarlat.jm@gmail.com>
2017-03-07 00:07:16 +0200

    added Test_2_10()

--- JuniorMindHomeworks/BaseTwo/BaseTwo.cs
+++ JuniorMindHomeworks/BaseTwo/BaseTwo.cs
@@ -4,11 +4,12 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace BaseTwo
 {
     [TestClass]
-    public class UnitTest1
+    public class BaseTwo
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_2_10()
         {
+            Assert.AreEqual({'1','0'}, DecimalToBaseTwo(2));
         }
     }
 }
